b196872615879f4799833c21b7dd21e3d2a52045
Andrei Stratu <andrew.avantgardian@gmail.com>
2022-03-24 23:55:13 +0000

    Update summit_StreamlabsSystem.py
    Added boilerplate to main branch

---
+++ summit_StreamlabsSystem.py
@@ -0,0 +1,16 @@
+ScriptName = "The Summit"
+Website = "https://"
+Description = "Minigame about climbing the stairs"
+Creator = "Avantgardian"
+Version = "0.1"
+
+def Init():
+    return
+
+
+def Execute(data):
+    return
+
+
+def Tick():
+    return
