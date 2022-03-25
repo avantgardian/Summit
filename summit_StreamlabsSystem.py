170856aab943baf0039aa0d07ba727352c5515fc
Andrei Stratu <andrew.avantgardian@gmail.com>
2022-03-25 20:06:15 +0000

    Update summit_StreamlabsSystem.py
    messaging methods

--- summit_StreamlabsSystem.py
+++ summit_StreamlabsSystem.py
@@ -9,8 +9,21 @@ def Init():
 
 
 def Execute(data):
+    if data.GetParam(0) != "!pyramid":
+        return
     return
 
 
 def Tick():
+    return
+
+
+def is_pyramid():
+    pyramid_probability = 10
+    random_chance = Parent.GetRandom(0, 100)
+    return random_chance <= pyramid_probability
+
+
+def send_message(message):
+    Parent.SendStreamMessage(message)
     return
